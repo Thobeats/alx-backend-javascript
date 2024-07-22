43cde86490202f6449f55e900c4e2810bc16f92e
Iyanu Dayisi <tobiy23@gmail.com>
2024-07-22 18:43:17 +0100

    changed let to const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-
-    return [task, task2];
-  }
+  const task = !trueOrFalse;
+  const task2 = !!trueOrFalse;
 
   return [task, task2];
 }
