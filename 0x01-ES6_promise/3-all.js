6c600b3e75f8c45b97e24e753e0d861091c8708a
Iyanu Dayisi <tobiy23@gmail.com>
2024-07-25 00:06:11 +0100

    all Promises and utils

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,10 @@
 import { uploadPhoto, createUser } from './util';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((res) => console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`))
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
     .catch(() => console.log('Signup system offline'));
 }
