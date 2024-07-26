1d300a5f25198c227869f1f4b0fdea1c3a52e520
Iyanu Dayisi <tobiy23@gmail.com>
2024-07-26 07:11:51 +0100

    initialize three class rooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const classRooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(24),
+  ];
+
+  return classRooms;
+}
