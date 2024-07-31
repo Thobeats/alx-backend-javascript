fb41b5cb55bd261caf17686c5db4d914cb02c278
Iyanu Dayisi <tobiy23@gmail.com>
2024-07-31 05:20:48 +0100

    update grade by city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,11 +3,12 @@ function updateStudentGradeByCity(arr, city, newGrades) {
     const filteredStudents = arr.filter((obj) => obj.location === city, city);
     return filteredStudents.map((student) => {
       const grade = newGrades.filter((grade) => grade.studentId === student.id, student);
+      const studentGrade = grade.length > 0 ? grade[0].grade : 'N/A';
       return {
         id: student.id,
         firstName: student.firstName,
         location: student.location,
-        grade,
+        grade: studentGrade,
       };
     }, newGrades);
   }
