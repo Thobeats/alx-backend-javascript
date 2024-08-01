3dd4e8bba5292f5d9df253fe46716770a38122ad
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-01 07:05:26 +0100

    Student Class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -12,9 +12,37 @@ export interface Director extends Teacher {
 }
 
 export interface printTeacherFunction {
-    
+
 }
 
 export const printTeacher = (firstName: string, lastName: string): string => {
     return `${firstName[0]}. ${lastName}`;
+}
+
+export interface IStudentClassConstructor {
+    new (firstName: string, lastName: string): IStudentClass;
+}
+  
+export interface IStudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+export class StudentClass {
+    private _firstName: string;
+    private _lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    workOnHomework(): string{
+        return "Currently working";
+    }
+
+    displayName() {
+        return this._firstName;
+    }
 }
