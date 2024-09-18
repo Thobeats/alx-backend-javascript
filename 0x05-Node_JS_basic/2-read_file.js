af11c0f4a23468d53a9a993d2287fc633984df6f
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-18 21:03:17 +0100

    read file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+
+const countStudents = (path) => {
+  try {
+    const database = fs.readFileSync(path, 'utf-8');
+    const lines = database.trim().split('\n');
+    const NUMBER_OF_STUDENTS = lines.length - 1;
+    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+    lines.shift();
+    const FIELDS = {};
+    lines.forEach((line) => {
+      const student = line.split(',');
+      if (!FIELDS[student[3]]) FIELDS[student[3]] = [];
+      FIELDS[student[3]].push(student[0]);
+    });
+
+    for (const FIELD in FIELDS) {
+      if (FIELD) {
+        console.log(`Number of students in ${FIELD}: ${FIELDS[FIELD].length}. List: ${FIELDS[FIELD].join(', ')}`);
+      }
+    }
+  } catch (err) {
+    console.log('Cannot load database');
+  }
+};
+
+module.exports = countStudents;
