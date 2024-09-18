b13094542b97729274be032a20526ee4308696e1
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-18 22:05:24 +0100

    read file async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,30 @@
+const fs = require('fs/promises');
+
+async function countStudents(path){
+  try {
+    const database = await fs.readFile(path, { encoding: 'utf8' });
+    const lines = database.trim().split('\n');
+    lines.shift();
+    const FIELDS = {};
+    let NUMBER_OF_STUDENTS = 0;
+    lines.forEach((line) => {
+      const student = line.split(',');
+      if (student[3] === undefined) return;
+      if (!FIELDS[student[3]]) FIELDS[student[3]] = [];
+      FIELDS[student[3]].push(student[0]);
+      NUMBER_OF_STUDENTS += 1;
+    });
+
+    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+    for (const FIELD in FIELDS) {
+      if (FIELD) {
+        console.log(`Number of students in ${FIELD}: ${FIELDS[FIELD].length}. List: ${FIELDS[FIELD].join(', ')}`);
+      }
+    }
+  } catch (err) {
+    console.log("Cannot load the database");
+  }
+};
+
+module.exports = countStudents;
