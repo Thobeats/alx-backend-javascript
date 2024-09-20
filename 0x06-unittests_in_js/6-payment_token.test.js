ac3abea835c726ce4888a4e209b6821ed884e0cd
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-20 18:51:31 +0100

    getPaymentTokenFromAPI

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,11 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('get the result of getPaymentTokenFromAPI(true)', () => {
+  it('get the result of getPaymentTokenFromAPI(true)', (done) => {
     getPaymentTokenFromAPI(true)
       .then((data) => {
         expect(data.data).to.equal('Successful response from the API');
+        done();
       });
   });
 });
