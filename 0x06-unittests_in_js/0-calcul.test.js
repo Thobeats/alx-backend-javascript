faa103794e126b261ed7ebe730dd3412ec0c15ca
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-20 16:01:45 +0100

    test calculateNumber

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,8 +4,8 @@ const assert = require('assert');
 describe("calculateNumber", function() {
     it("checks equality", function() {
         assert.equal(calculateNumber(1, 3), 4);
-        assert.equal(calculateNumber(1.5, 3.2), 5);
+        assert.equal(calculateNumber(1, 3.7), 5);
+        assert.equal(calculateNumber(1.2, 3.7), 5);
         assert.equal(calculateNumber(1.5, 3.7), 6);
-        assert.equal(calculateNumber(3, 3.7), 7);
     });
   });
