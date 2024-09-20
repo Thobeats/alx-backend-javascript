643f4b2d02bec80ee6415ce730de9bc55f74f4c0
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-20 15:56:14 +0100

    calculate with type

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,11 @@
+const calculateNumber = require('./1-calcul.js');
+const assert = require('assert');
+
+describe("calculateNumber", function() {
+    it("checks equality", function() {
+        assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+});
