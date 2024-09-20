ad27690902fcfddbda0168359fb43e7293496671
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-20 22:28:10 +0100

    api test with parameter

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,42 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+    it('get the correct status code', ()=>{
+        request('http://localhost:7865', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+        });
+    });
+
+    it('get the correct result', ()=>{
+        request('http://localhost:7865', (error, response, body) => {
+            expect(response.body).to.equal('Welcome to the payment system');
+        });
+    });
+
+    it('get the correct port', ()=>{
+        request('http://localhost:7865', (error, response, body) => {
+            expect(response.request.uri.port).to.equal('7865');
+        });
+    });
+
+    it('get the correct value for id', ()=>{
+        request('http://localhost:7865/cart/12', (error, response, body) => {
+            expect(body).to.equal('Payment methods for cart 12');
+        });
+    });
+
+    it('get the correct status code', ()=>{
+        request('http://localhost:7865/cart/12', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+        });
+    });
+
+    it('get the correct value for wrong value', ()=>{
+        request('http://localhost:7865/cart/hello', (error, response, body) => {
+            expect(response.statusCode).to.equal(404);
+        });
+    });
+
+
+});
