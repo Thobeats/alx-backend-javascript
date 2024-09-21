50cc96e446b01cb22076fff403091bbca1e48b7e
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-21 14:11:25 +0100

    api test

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const app = express();
-const port = 7865;
+const PORT = 7865;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
@@ -13,21 +15,22 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${id}`);
 });
 
-app.get('/available_payments', (req, res) => {
-  res.send({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
-  });
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
 app.post('/login', (req, res) => {
-  const username = req.body.userName;
+  let username = '';
 
-  res.send(`Welcome ${username}`)
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
 });
 
-app.listen(port, () => {
-  console.log('API available on localhost port 7865');
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+module.exports = app;
